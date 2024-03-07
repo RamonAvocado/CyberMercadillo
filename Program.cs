55a03c5bfcbd0e25408615caee718502913c18f8
RamonAvocado <ramonet.miret@gmail.com>
2024-03-07 16:27:44 +0100

    Cambio program.cs

--- Program.cs
+++ Program.cs
@@ -29,7 +29,7 @@ builder.Services.AddCors(options =>
 {
     options.AddPolicy("AllowSpecificOrigin", builder =>
     {
-        builder.WithOrigins("http://127.0.0.1:5500");
+        builder.WithOrigins("https://ramonavocado.github.io/CyberMercadillo/");
         builder.AllowAnyHeader();
         builder.AllowAnyMethod();
     });
