116c261bfa505e8d375d2a276575d564c0ff4971
Hokawokho <jordipeiropuig@gmail.com>
2024-06-02 15:19:01 +0200

    Interfaz Final

--- Interfaces/Metodos/MetodosUser.js
+++ Interfaces/Metodos/MetodosUser.js
@@ -108,7 +108,17 @@ async function agregarCerrarSesion() {
         } else { 
             sesionUserLink.innerHTML = '<button class="styled-button" onclick="location.href=\'./InterfazComprador.html\'">Mi Cuenta</button>';
             sesionUserLink.innerHTML += '<button class="styled-button" onclick="location.href=\'./ListaDeseados.html\'">Lista de Deseados</button>';
-            sesionSUserLink.innerHTML += '<button class="styled-button" onclick="location.href=\'./CarritoDeCompra.html\'" style="margin-left:auto;">Carrito de Compra</button>';
+            sesionSUserLink.innerHTML = `
+                <div data-tooltip="Carrito de compra" class="boton-carro" onclick="location.href='./CarritoDeCompra.html'">
+                    <div class="button-wrapper">
+                        <div class="text">CARRITO DE COMPRA</div>
+                        <span class="icon">
+                            <svg viewBox="0 0 16 16" class="bi bi-cart2" fill="currentColor" height="16" width="16" xmlns="http://www.w3.org/2000/svg">
+                                <path d="M0 2.5A.5.5 0 0 1 .5 2H2a.5.5 0 0 1 .485.379L2.89 4H14.5a.5.5 0 0 1 .485.621l-1.5 6A.5.5 0 0 1 13 11H4a.5.5 0 0 1-.485-.379L1.61 3H.5a.5.5 0 0 1-.5-.5zM3.14 5l1.25 5h8.22l1.25-5H3.14zM5 13a1 1 0 1 0 0 2 1 1 0 0 0 0-2zm-2 1a2 2 0 1 1 4 0 2 2 0 0 1-4 0zm9-1a1 1 0 1 0 0 2 1 1 0 0 0 0-2zm-2 1a2 2 0 1 1 4 0 2 2 0 0 1-4 0z"></path>
+                            </svg>
+                        </span>
+                    </div>
+                </div>`;
         }      
     }
 }
@@ -206,6 +216,7 @@ function mostrarPedidos(carritos){
         tabla.appendChild(carrito);
     }
 }
+    
 
 
     async function mostrarVendedor() {
